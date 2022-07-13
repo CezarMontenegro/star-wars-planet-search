302f838b2ac9a1c01646a3d4c87281bc07c2654f
Cezar <cezarguimaraes@gmail.com>
2022-07-13 17:02:59 -0300

    Tentar fazer logica para filtros por quantidade funcionarem

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,33 @@ function App() {
   return (
     <ContextProvider>
       <div>
-        <h1>Star Wars Planets</h1>
-        <input
-          type="text"
-          name="name"
-          onChange={ (e) => handleFilter(e) }
-          data-testid="name-filter"
-        />
+        <div>
+          <h1>Star Wars Planets</h1>
+          <input
+            type="text"
+            name="name"
+            onChange={ (e) => handleFilter(e) }
+            data-testid="name-filter"
+          />
+        </div>
+        <div>
+          <select data-testid="column-filter">
+            <option>population</option>
+            <option>orbital_period</option>
+            <option>diameter</option>
+            <option>rotation_period</option>
+            <option>surface_water</option>
+          </select>
+          <select data-testid="comparison-filter">
+            <option>maior que</option>
+            <option>menor que</option>
+            <option>igual a</option>
+          </select>
+          <input type="number" data-testid="button-filter" />
+          <button type="button">
+            Filtrar
+          </button>
+        </div>
         <Table filter={ filter } />
       </div>
     </ContextProvider>
